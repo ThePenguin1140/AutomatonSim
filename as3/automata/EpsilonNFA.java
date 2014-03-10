153ed0e4178e0efb55e9ef8fba9745afe8b5d9e0
Jason Wuertz <jason@wuertz.net>
2014-03-10 17:56:14 -0300

    made subset names pretty

--- as3/automata/EpsilonNFA.java
+++ as3/automata/EpsilonNFA.java
@@ -168,6 +168,7 @@ public class EpsilonNFA extends Automaton {
 		String name = "";
 		for(State s: set)
 			name += s.getName()+",";
+		name = name.substring(0, name.length()-2);
 		return name;
 	}
 }
